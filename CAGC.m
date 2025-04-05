39d9e8e992993b2131a2d074feb963178385d59e
Eatlinyas <138202796+Eatlinyas@users.noreply.github.com>
2025-04-05 15:34:19 +0800

    Add files via upload

--- CAGC.m
+++ CAGC.m
@@ -1,20 +1,86 @@
 clear,clc;
 % CAGC : C/A code generate and capture
-% option ---------------------------------------------
-Local_Prn    = 2;               % 待生成C/A码的卫星序列号，其C/A码储存在Local_CAcode中
-Contrast_Prn = 1;               % 与Local进行C/A码互相关的卫星序列号，其C/A码储存在Contrast_CAcode中
+% option ----------------------------------------------------------
+Local_Prn    = 10;              % 待生成C/A码的卫星序列号，其C/A码储存在Local_CAcode中
+Contrast_Prn = 10;              % 与Local进行C/A码互相关的卫星序列号，其C/A码储存在Contrast_CAcode中
+Sampling_Freq= 16368000;        % 采样率:16.368MHz
 Intermediate_Freq = 4092000;    % 理论中频:4.092MHz
-Step_Size    = 500;            % 搜索步长:Hz
-Search_Scope = 0000;           % 搜索范围:±Hz
-% C/A code generate
-Local_CAcode = CAcodeGenerate(Local_Prn);
-Contrast_CAcode = CAcodeGenerate(Contrast_Prn);
-function CAcode = CAcodeGenerate(Prn)
+Step_Size    = 25;             % 搜索步长:Hz
+Search_Scope = 5000;            % 搜索范围:±Hz
+CA_Mode      = 00;              % C/A码表现形式，0:1或0/-1：1或-1
+IfPaint      = 0;               % 是否画互相关图，1：是/0：否
+load gps_data_20ms.mat;         % 打开文件
+% C/A code generate 使用抽头的组合选择生成C/A------------------------
+Local_CAcode = CAcodeGenerate(Local_Prn,CA_Mode);
+Contrast_CAcode = CAcodeGenerate(Contrast_Prn,CA_Mode);
+
+% Cross Correlation------------------------------------------------
+Cor = Cross_Correlation(Local_CAcode,Contrast_CAcode);
+% painting---------------------------------------------------------
+if(IfPaint)
+    x=-511:1:511;
+    plot(x,Cor,'.-');      % 画折线图
+    grid on;               % 添加网格
+    title('互相关图');
+    xlabel('偏移量');
+    ylabel('值');
+end
+% capture----------------------------------------------------------
+% Result_FFT = fft(gps_dat);
+% Amp_FFT = abs(Result_FFT);
+% plot(Amp_FFT);
+
+N = 327360;          
+Fs = Sampling_Freq;      
+df = Fs / N;         % 频率分辨率 = 50 Hz
+f = (0:N-1) * df; 
+
+for i=1:1:N
+    if(gps_dat(i)>0)
+        gps_dat(i)=gps_dat(i)-1;
+    else
+        gps_dat(i)=gps_dat(i)+1;
+    end
+end
+
+% 计算 FFT
+Y = abs(fft(gps_dat));
+Y = Y(1:N/2); % 只取正频率部分
+f = f(1:N/2); % 频率轴对应
+threshold = max(Y) * 0.1;
+% 找到主峰
+[peaks, locs] = findpeaks(Y, 'MinPeakHeight', threshold);
+freqs = f(locs);  % 主频点
+% 绘制
+plot(f, Y);
+hold on;
+plot(freqs, peaks, 'r*'); % 标出峰值
+hold off;
+% C/A捕获
+CA_Freq = max(freqs);
+Step = Sampling_Freq/CA_Freq;
+t = (0:length(Y)-1)/Sampling_Freq;  % 时间轴
+% 下变频，将 4.42 MHz 信号移到基带
+Y = Y';
+Y_baseband = Y .* exp(-1j * 2 * pi * CA_Freq * t)';  
+
+% 低通滤波，去掉高频分量
+LPF = designfilt('lowpassfir', 'FilterOrder', 100, ...
+                 'CutoffFrequency', 1e6, ...  % 设定低通截止频率
+                 'SampleRate', Sampling_Freq);
+Y_filtered = filter(LPF, real(Y_baseband));
+
+% Y_filtered = filtfilt(LPF, real(Y_baseband)); 
+% freqz(LPF);  % 查看滤波器频率响应
+
+
+% function---------------------------------------------------------
+function CAcode = CAcodeGenerate(Prn,CA_Mode)
     CAcode = zeros(1023,1);
     G1=ones(10,1);%状态
     G2=ones(10,1);
-    G1_Code=-1*ones(1023,1);%序列
-    G2_Code=-1*ones(1023,1);
+    G1_Code=-2*ones(1023,1);%序列
+    G2_Code=-2*ones(1023,1);
     Prn_Selection = [
         2, 6;   % PRN1
         3, 7;   % PRN2
@@ -49,40 +115,9 @@ function CAcode = CAcodeGenerate(Prn)
         3, 8;   % PRN31
         4, 9;   % PRN32
     ];
-    Prn_Offset=[
-        5;
-        6;
-        7;
-        8;
-        17;
-        18;
-        139;
-        140;
-        141;
-        251;
-        252;
-        254;
-        255;
-        256;
-        257;
-        258;
-        469;
-        470;
-        471;
-        472;
-        473;
-        474;
-        509;
-        512;
-        513;
-        514;
-        515;
-        516;
-        859;
-        860;
-        861;
-        862;
-    ];
+    if ((Prn>32) || (Prn<1))
+	    error('Prn must be a number between 1 and 32\n')
+    end
     for i=1:1023
         %异或运算
         G1_temp=XOR(G1(3),G1(10));
@@ -93,11 +128,7 @@ function CAcode = CAcodeGenerate(Prn)
         G2_temp=XOR(G2_temp,G2(10));
         %输出
         G1_Code(i)=G1(10);
-        if(i+Prn_Offset(Prn)<=1023)
-            G2_Code(i+Prn_Offset(Prn))=XOR(G2(Prn_Selection(Prn,1)),G2(Prn_Selection(Prn,2)));
-        else
-            G2_Code(i+Prn_Offset(Prn)-1023)=XOR(G2(Prn_Selection(Prn,1)),G2(Prn_Selection(Prn,2)));
-        end
+        G2_Code(i)=XOR(G2(Prn_Selection(Prn,1)),G2(Prn_Selection(Prn,2)));
         %寄存器移位
         for j=10:-1:2
             G1(j)=G1(j-1);
@@ -106,18 +137,52 @@ function CAcode = CAcodeGenerate(Prn)
         G1(1)=G1_temp;
         G2(1)=G2_temp;
     end
+    %G1，G2组合生成C/A码
     for i = 1:1023
         CAcode(i)=XOR(G1_Code(i),G2_Code(i));
+    end
+    if(CA_Mode==-1)
+        CAcode = Zero2MinusOne(CAcode);
     end
 end
 
 function Result = XOR(one,two)
     if(one*two<0)
-        error=1
+        error('XOR error\n')
     end
     if(one~=two)
         Result=1;
     else
         Result=0;
+    end
+end
+
+function New_CA = Zero2MinusOne(CA)
+    New_CA=zeros(1023,1);
+    for i=1:1:1023
+        if(CA(i)==0)
+            New_CA(i)=-1;
+        else
+            New_CA(i)=1;
+        end
+    end
+end
+
+function Result = Cross_Correlation(Local,Contrast)
+    Local=Zero2MinusOne(Local);
+    Contrast=Zero2MinusOne(Contrast);
+    Result = zeros(1023,1);
+    for offset = -511:1:511
+        for i=1:1:1023
+            if(i+offset>1023)
+                Cur=i+offset-1023;
+            elseif(i+offset<=0)
+                Cur=i+offset+1023;
+            else
+                Cur=i+offset;
+            end
+            Result(offset+511+1) = Result(offset+511+1) + Local(Cur)*Contrast(i);
+        end
+        Result(offset+511+1) = Result(offset+511+1) / 1023;
     end
 end
